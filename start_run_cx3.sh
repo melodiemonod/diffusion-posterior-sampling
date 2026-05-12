5e697c793776584d7e2920cec8c34c012d9d3cc8
Melodie Monod <monod.melodie@gmail.com>
2026-05-12 15:16:17 +0200

    start run cx3

--- start_run_cx3.sh
+++ start_run_cx3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-N=2  # number of jobs you want to launch
+N=200  # number of jobs you want to launch
 
 DATANAME="imagenet"
 OPERTORNAME="inpainting"
 BASE_JOBID="${DATANAME}/${OPERTORNAME}"
 
-REPO_DIR="/rds/general/user/mm3218/home/git/diffusion-posterior-sampling"
+REPO_DIR="rds/general/user/mm3218/home/git/diffusion-posterior-sampling"
 SAVE_DIR="/rds/general/user/mm3218/home/projects/2026/dps_sbc/${BASE_JOBID}"
 
 data_config=$REPO_DIR/configs/imagenet_data_config.yaml
@@ -53,9 +53,54 @@ python3 \$REPO_DIR/sample_condition.py \\
 EOF
 
     chmod +x $job_script
-    cd $job_dir
-    qsub $job_script
-
-    echo "Submitted job $jobid"
-
+    
 done
+
+
+for i in $(seq 1 $((N / 50))); do
+    start_index=$(( (i - 1) * 50 + 1 ))
+    end_index=$(( i * 50 ))
+
+    job_script="${SAVE_DIR}/job_run_${start_index}-${end_index}.pbs"
+
+    cat > $job_script <<EOF
+#!/bin/sh
+#PBS -N dps_${start_index}-${end_index}
+#PBS -l walltime=72:00:00
+#PBS -l select=1:ncpus=1:mem=100gb:ngpus=1
+#PBS -J ${start_index}-${end_index}
+#PBS -j oe
+
+source /rds/general/user/mm3218/home/miniforge3/etc/profile.d/conda.sh
+conda activate DPS
+
+REPO_DIR="$REPO_DIR"
+data_config="$data_config"
+model_config="$model_config"
+diffusion_config="$diffusion_config"
+task_config="$task_config"
+
+save_dir=${SAVE_DIR}/run_\${PBS_ARRAY_INDEX}
+
+cd \$REPO_DIR
+python3 \$REPO_DIR/sample_condition.py \\
+  --data_config=\$data_config \\
+  --model_config=\$model_config \\
+  --diffusion_config=\$diffusion_config \\
+  --task_config=\$task_config \\
+  --gpu=0 \\
+  --save_dir=\$save_dir
+EOF
+
+    chmod +x $job_script
+    job_name=$(basename "$job_script")
+    if [ "$i" -eq 1 ]; then
+        echo "cd $SAVE_DIR"
+        echo "qsub $job_name"
+    else
+        echo "qsub -W depend=afterany:12345 $job_name"
+    fi
+    
+done
+
+
